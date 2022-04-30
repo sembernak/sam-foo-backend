4120879abba106c120f779992ffd5d9724a2321e
sammyb102 <samcori@udel.edu>
2022-04-30 17:17:34 -0400

    testing ver

--- src/index.js
+++ src/index.js
@@ -5,46 +5,53 @@ const writer = require('csv-to-sql-script');
 const sqlite3 = require('sqlite3');
 const fs = require("fs");
 
-//Defining Express App
-const app = express();
-
 //Defining arrays to hold endpoint info
 const dfEnd = [];
 const fullEnd = [];
 const infoEnd = [];
 
-//Adding Morgan to app to handle HTTP requests
-app.use(morgan('combined'));
-
 //Re-writing CSV as SQL
 writer.writeMigration('resources/codefoobackend_cfgames.csv', 'resources/dbinfo.sql', 'media');
-const commands = fs.readFileSync("resources/dbinfo.sql").toString();
-const commandsLine1 = commands.substring(0, commands.indexOf("INSERT") - 1);
-const commandsLine2 = commands.substring(commands.indexOf("INSERT"));
 
-//Create and populate internal sqlite database via SQL script
-const db = new sqlite3.Database('resources/dbinfo.db');
-db.serialize(() => {
-  db.run("DROP TABLE IF EXISTS media");
-  db.run(commandsLine1);
-  db.run(commandsLine2);
+//Defining Express App
+const app = express();
 
-  //Retrieving JSON for default (empty) endpoint from database
-  db.each("SELECT rowid as id, name FROM media", (err, row) => {
-    dfEnd.push({id: row.id, name: row.name});
+//Retrieving SQL commands
+var commands = "";
+fs.watchFile('resources/dbinfo.sql', function (event, trigger) {
+  commands = fs.readFileSync("resources/dbinfo.sql").toString();
+
+  const commandsLine1 = commands.substring(0, commands.indexOf("INSERT") - 1);
+  const commandsLine2 = commands.substring(commands.indexOf("INSERT"));
+
+  //Adding Morgan to app to handle HTTP requests
+  app.use(morgan('combined'));
+
+  //Create and populate internal sqlite database via SQL script
+  const db = new sqlite3.Database('resources/dbinfo.db');
+  db.serialize(() => {
+    db.run("DROP TABLE IF EXISTS media");
+    db.run(commandsLine1);
+    db.run(commandsLine2);
+
+    //Retrieving JSON for default (empty) endpoint from database
+    db.each("SELECT rowid as id, name FROM media", (err, row) => {
+      dfEnd.push({id: row.id, name: row.name});
+    });
+
+    //Retrieving JSON for full endpoint from database
+    db.each("SELECT * FROM media", (err, row) => {
+      fullEnd.push({id: row.id, media_type: row.media_type, name: row.name, short_name: row.short_name, long_description: row.long_description, short_description: row.short_description, created_at: row.created_at, updated_at: row.updated_at, review_url: row.review_url, review_score: row.review_score, slug: row.slug, genres: row.genres, created_by: row.created_by, published_by: row.published_by, franchises: row.franchises, regions: row.regions});
+    })
+
+    //Retrieving JSON for info endpoint from database
+    db.each("SELECT * FROM media", (err, row) => {
+      infoEnd.push({id: row.id, media_type: row.media_type, name: row.name, short_name: row.short_name, long_description: row.long_description, short_description: row.short_description, created_at: row.created_at, updated_at: row.updated_at, slug: row.slug, genres: row.genres, created_by: row.created_by, published_by: row.published_by, franchises: row.franchises, regions: row.regions});
+    })
   });
-
-  //Retrieving JSON for full endpoint from database
-  db.each("SELECT * FROM media", (err, row) => {
-    fullEnd.push({id: row.id, media_type: row.media_type, name: row.name, short_name: row.short_name, long_description: row.long_description, short_description: row.short_description, created_at: row.created_at, updated_at: row.updated_at, review_url: row.review_url, review_score: row.review_score, slug: row.slug, genres: row.genres, created_by: row.created_by, published_by: row.published_by, franchises: row.franchises, regions: row.regions});
-  })
-
-  //Retrieving JSON for info endpoint from database
-  db.each("SELECT * FROM media", (err, row) => {
-    infoEnd.push({id: row.id, media_type: row.media_type, name: row.name, short_name: row.short_name, long_description: row.long_description, short_description: row.short_description, created_at: row.created_at, updated_at: row.updated_at, slug: row.slug, genres: row.genres, created_by: row.created_by, published_by: row.published_by, franchises: row.franchises, regions: row.regions});
-  })
-})
-
+  //Close database
+  db.close();
+});
 //Name and id from every media
 app.get('/', (req, res) => {
     res.send(JSON.stringify(dfEnd));
@@ -111,6 +118,3 @@ app.get('/info/genre/:genreName', (req, res) => {
 app.listen(3000, () => {
   console.log('listening on port 3000');
 });
-
-//Close database
-db.close();
